97435ad28a8fb70b3a3826175ce4596f7937e9c1
arata <contact@materializing.net>
2016-03-19 14:47:25 +0900

    add スキーマ

---
+++ Config/Schema/callable_editor_templates.php
@@ -0,0 +1,29 @@
+<?php 
+class CallableEditorTemplatesSchema extends CakeSchema {
+
+	public $file = 'callable_editor_templates.php';
+
+	public $connection = 'plugin';
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $callable_editor_templates = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
+		'model' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'モデル名', 'charset' => 'utf8'),
+		'model_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => 'モデルID'),
+		'editor_template_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => 'エディターテンプレートID'),
+		'display_before' => array('type' => 'boolean', 'null' => true, 'default' => '0', 'comment' => 'コンテンツ上部に表示する'),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+}
---
+++ Config/init.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * [Config] CallableEditorTemplate プラグイン用
+ * データベース初期化
+ */
+$this->Plugin->initDb('plugin', 'CallableEditorTemplate');
