186d6b56ef6678d777d98843415fcc78fba22a41
arata <contact@materializing.net>
2016-03-27 08:04:16 +0900

    fixes エレメントビューにサンプルコード記載。表示内容をエディタテンプレートの内容のみに変更

--- View/Elements/callable_editor_template.php
+++ View/Elements/callable_editor_template.php
@@ -8,9 +8,12 @@
  * @license			MIT
  */
 $callableEditorTemplateData = Hash::get($this->request->data, 'CallableEditorTemplateData');
+// 表示用サンプルコード
+// エディタテンプレート名: Hash::get($callableEditorTemplateData, 'EditorTemplate.name')
+// エディタテンプレート説明文: Hash::get($callableEditorTemplateData, 'EditorTemplate.description')
+// エディタテンプレート内容: Hash::get($callableEditorTemplateData, 'EditorTemplate.html')
+// エディタテンプレート用画像: $this->BcUpload->uploadImage('EditorTemplate.image', Hash::get($callableEditorTemplateData, 'EditorTemplate.image'), array('link' => false))
 ?>
 <div id="CallableEditorTemplateContentBox">
-	<h3><?php echo Hash::get($callableEditorTemplateData, 'EditorTemplate.name') ?></h3>
-	<?php echo $this->BcUpload->uploadImage('EditorTemplate.image', Hash::get($callableEditorTemplateData, 'EditorTemplate.image'), array('link' => false)) ?>
 	<?php echo Hash::get($callableEditorTemplateData, 'EditorTemplate.html') ?>
 </div>
