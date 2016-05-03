38231e13b85370cbfef0d3051d1e9efaa850a64a
arata <contact@materializing.net>
2016-05-03 18:13:14 +0900

    [WIP] Test Event for CallableEditorTemplateHelperEventListener

--- Test/Case/Event/CallableEditorTemplateHelperEventListenerTest.php
+++ Test/Case/Event/CallableEditorTemplateHelperEventListenerTest.php
@@ -11,9 +11,17 @@
 class CallableEditorTemplateHelperEventListenerTest extends BaserTestCase
 {
 
-	public function test_formAfterForm()
+	public function testFormAfterForm()
 	{
-		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+//		$this->markTestIncomplete('このテストは、まだ実装されていません。');
+//		App::uses('CakeEventManager', 'Event');
+//		$events = CakeEventManager::instance();
+
+		$expected = 'pages';
+		$this->assertContains($expected, array('pages', 'blog_posts'));
+
+		$expected = 'blog_posts';
+		$this->assertContains($expected, array('pages', 'blog_posts'));
 	}
 
 }
