149a718f0f0d7a35e1fabb875fdfa3fa651f93f1
arata <contact@materializing.net>
2016-05-03 16:04:55 +0900

    add Test for CallableEditorTemplateConfig

--- Test/Case/Model/CallableEditorTemplateConfigTest.php
+++ Test/Case/Model/CallableEditorTemplateConfigTest.php
@@ -34,6 +34,9 @@ class CallableEditorTemplateConfigTest extends BaserTestCase
 		parent::tearDown();
 	}
 
+	/**
+	 * 正常系
+	 */
 	public function test正常チェック()
 	{
 		$this->CallableEditorTemplateConfig->create(array(
@@ -47,6 +50,68 @@ class CallableEditorTemplateConfigTest extends BaserTestCase
 
 		$this->assertTrue($this->CallableEditorTemplateConfig->validates());
 		$this->assertEmpty($this->CallableEditorTemplateConfig->validationErrors);
+	}
+
+	/**
+	 * 異常系
+	 */
+	public function testValidateNotEmpty()
+	{
+		$this->CallableEditorTemplateConfig->create(array(
+			'CallableEditorTemplateConfig' => array(
+				'model'		 => '',
+				'content_id' => '',
+				'title'		 => '',
+				'status'	 => '',
+			)
+		));
+		$this->assertFalse($this->CallableEditorTemplateConfig->validates());
+
+		$expected = array(
+			'model' => array('モデル名を入力してください。'),
+		);
+		$this->assertEquals($expected, $this->CallableEditorTemplateConfig->validationErrors);
+	}
+
+	/**
+	 * 異常系
+	 */
+	public function testValidateMaxLength()
+	{
+		$this->CallableEditorTemplateConfig->create(array(
+			'CallableEditorTemplateConfig' => array(
+				'model'		 => 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああ',
+				'content_id' => '',
+				'title'		 => '',
+				'status'	 => '',
+			)
+		));
+		$this->assertFalse($this->CallableEditorTemplateConfig->validates());
+
+		$expected = array(
+			'model' => array('モデル名は255文字以内で入力してください。'),
+		);
+		$this->assertEquals($expected, $this->CallableEditorTemplateConfig->validationErrors);
+	}
+
+	/**
+	 * 正常系
+	 */
+	public function testGetDefaultValue()
+	{
+		$result		 = $this->CallableEditorTemplateConfig->getDefaultValue();
+		$expected	 = array(
+			'CallableEditorTemplateConfig' => array(
+				'status' => false,
+				'title'	 => Configure::read('CallableEditorTemplate.label_name'),
+			),
+		);
+		$this->assertEquals($expected, $result, '初期値が正しくありません。');
 	}
 
 }
--- Test/Fixture/Model/CallableEditorTemplateFixture.php
+++ Test/Fixture/Model/CallableEditorTemplateFixture.php
@@ -28,10 +28,20 @@ class CallableEditorTemplateFixture extends BaserTestFixture
 			'model'				 => 'BlogPost',
 			'model_id'			 => '1',
 			'editor_template_id' => '1',
-			'display_before'	 => 1,
+			'display_before'	 => 0,
 			'status'			 => 1,
 			'modified'			 => null,
-			'created'			 => '2016-05-01 09:32:30',
+			'created'			 => '2016-05-02 09:32:30',
+		),
+		array(
+			'id'				 => '3',
+			'model'				 => 'BlogPost',
+			'model_id'			 => '2',
+			'editor_template_id' => '2',
+			'display_before'	 => 1,
+			'status'			 => 0,
+			'modified'			 => null,
+			'created'			 => '2016-05-03 09:32:30',
 		),
 	);
 
