cbe0d4ae44f5d59d1eaeef5f6d218716f335f6a8
arata <contact@materializing.net>
2016-05-03 16:51:22 +0900

    add Test Model for CallableEditorTemplate

--- Test/Case/Model/CallableEditorTemplateTest.php
+++ Test/Case/Model/CallableEditorTemplateTest.php
@@ -34,6 +34,9 @@ class CallableEditorTemplateTest extends BaserTestCase
 		parent::tearDown();
 	}
 
+	/**
+	 * 正常系
+	 */
 	public function test正常チェック()
 	{
 		$this->CallableEditorTemplate->create(array(
@@ -48,6 +51,63 @@ class CallableEditorTemplateTest extends BaserTestCase
 
 		$this->assertTrue($this->CallableEditorTemplate->validates());
 		$this->assertEmpty($this->CallableEditorTemplate->validationErrors);
+	}
+
+	/**
+	 * 異常系
+	 */
+	public function testValidateNotEmpty()
+	{
+		$this->CallableEditorTemplate->create(array(
+			'CallableEditorTemplate' => array(
+				'model'		 => '',
+				'model_id'	 => '',
+			)
+		));
+		$this->assertFalse($this->CallableEditorTemplate->validates());
+
+		$expected = array(
+			'model' => array('モデル名を入力してください。'),
+		);
+		$this->assertEquals($expected, $this->CallableEditorTemplate->validationErrors);
+	}
+
+	/**
+	 * 異常系
+	 */
+	public function testValidateMaxLength()
+	{
+		$this->CallableEditorTemplate->create(array(
+			'CallableEditorTemplate' => array(
+				'model'		 => 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああああああいいいいいいいいいいううううううううううええええええええええおおおおおおおおおお'
+				. 'ああああああ',
+				'model_id'	 => '',
+			)
+		));
+		$this->assertFalse($this->CallableEditorTemplate->validates());
+
+		$expected = array(
+			'model' => array('モデル名は255文字以内で入力してください。'),
+		);
+		$this->assertEquals($expected, $this->CallableEditorTemplate->validationErrors);
+	}
+
+	/**
+	 * 正常系
+	 */
+	public function testGetDefaultValue()
+	{
+		$result		 = $this->CallableEditorTemplate->getDefaultValue();
+		$expected	 = array(
+			'CallableEditorTemplate' => array(
+				'display_before' => false,
+			),
+		);
+		$this->assertEquals($expected, $result, '初期値が正しくありません。');
 	}
 
 }
